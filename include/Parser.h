795797abd35f5ee41bc02f21294536a410931c62
Sebbah Nouri <87544655+nourisebbah@users.noreply.github.com>
2025-05-03 09:59:17 +0100

    Update Parser.h

--- include/Parser.h
+++ include/Parser.h
@@ -2,18 +2,39 @@
 #define PARSER_H
 
 #include "Tokenizer.h"
-#include <fstream>
+#include "VMWriter.h"
+#include <string>
 
 class Parser {
 private:
     Tokenizer& tokenizer;
-    std::ofstream outputFile;
+    VMWriter vmWriter;
+    std::string className;
 
-    void writeToken(const Token& token);
+    Token currentToken;
+    void advance();
+    bool check(const std::string& expected);
+    void expect(const std::string& expected);
+    std::string expectIdentifier();
+
+    void compileClass();
+    void compileClassVarDec();
+    void compileSubroutine();
+    void compileParameterList();
+    void compileVarDec();
+    void compileStatements();
+    void compileLet();
+    void compileIf();
+    void compileWhile();
+    void compileDo();
+    void compileReturn();
+    void compileExpression();
+    void compileTerm();
+    void compileExpressionList(int& nArgs);
 
 public:
     Parser(Tokenizer& tokenizer, const std::string& outputFilename);
     void parse();
 };
 
-#endif // PARSER_H
+#endif
